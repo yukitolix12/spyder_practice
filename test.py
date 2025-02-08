de1e9217cdbe95c447ade282d47633ed3bed2fe8
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2025-02-08 16:37:36 +0900

    サンプルデータを作成

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# データを読み込む
-data = pd.DataFrame({
-    "Year": [2020, 2021, 2022],
-    "Sales": [100, 150, 200]
-})
+# サンプルデータを作成
+data = {
+    "Month": ["January", "February", "March", "April", "May", "June"],
+    "Sales": [100, 120, 150, 170, 200, 220],
+    "Profit": [20, 25, 35, 40, 50, 60]
+}
 
 # データを表示
 print(data)
